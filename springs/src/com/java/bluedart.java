0a12fd3017b31633553c9e1f0fbfea637be8160b
sada <jon7sada@gmail.com>
2018-07-10 17:34:53 +0530

    com

--- springs/src/com/java/bluedart.java
+++ springs/src/com/java/bluedart.java
@@ -4,6 +4,7 @@ public class bluedart implements courier {
 
 	@Override
 	public String deliver(int orderid) {
+		System.out.println("hi");
 		// TODO Auto-generated method stub
 		return "bluedart is ready to deliver:::"+orderid;
 	}
